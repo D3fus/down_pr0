3ca5cdd6aa30249f21d55ba1b85ecedf29e53254
D3fus <marcin.maselko@gmail.com>
2018-03-30 16:52:04 +0200

    zu fix

--- down_pr0_ver5.py
+++ down_pr0_ver5.py
@@ -4,14 +4,12 @@ import requests
 import os
 
 def down(api_url_old, api_flag, ab, zu):
-	
 	if ab != "":
 		api_url_old = api_url + "&older=" + ab
-	
-	while True: 
-		
+	while True:
+
 		get_data = requests.get(api_url_old).json()
-		
+
 		id_nr = 0
 		url_nr = 0
 		while True:
@@ -19,7 +17,7 @@ def down(api_url_old, api_flag, ab, zu):
 				id = get_data["items"][id_nr]["id"]
 			except IndexError:
 				break
-			url = get_data["items"][url_nr]["image"] 
+			url = get_data["items"][url_nr]["image"]
 			end = url.split('.')[1]
 			full_name = str(id) + "." + end
 			full_url = "http://pr0gramm.com/data/images/" + url
@@ -35,14 +33,18 @@ def down(api_url_old, api_flag, ab, zu):
 			id_nr = id_nr + 1
 			url_nr = url_nr + 1
 			if zu == id:
+				print("Am ende von " + zu +" angekommen")
 				break
-		
-		if zu == id:
-			break
+
+
 		atEnd = get_data["atEnd"]
 		if atEnd == True:
+			print("am Ende angekommen/keine Bilder vorhanden mit diesem Parametern")
+			break
+		if zu == id:
 			break
 		api_url_old = api_url + "&older=" + str(id)
+
 
 main_api = "http://pr0gramm.com/api/items/get?self=true"
 print("Viel spaß mit dem Pr0gramm von D3fus")
